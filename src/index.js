77e40b9a270644d1b227c1bd6a794f17b38a3a30
Sourav <souravsirvastava027@gmail.com>
2021-01-31 12:27:03 +0530

    app-use-fix

--- src/index.js
+++ src/index.js
@@ -5,25 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const publicDirPath = path.join(__dirname,'../public')
 
-// writing multiples middleware and execute on each call
-const loadPublicDirPath = (req,res,next) =>{
-    res.send("yes")
-    next()
-}
-
-const logTheIncomingRequest = (req,res,next) =>{
-    console.log(`PATH ${req.originalUrl}`)
-    next()
-}
-
-const logTimeOfReq = (req,res,next) =>{
-    console.log(Date.now())
-    next()
-}
-
-middlewareFuncToBeExecuted = [loadPublicDirPath, logTheIncomingRequest, logTimeOfReq ]
-
-app.use(middlewareFuncToBeExecuted)
+app.use(express.static(publicDirPath))
 
 
 app.listen(PORT, ()=>{
